9d9b1cfa1783e86b374c069363ce45cf10e8f205
sannarino <einar.rahka@gmail.com>
2022-11-17 22:43:55 +0100

    Changed some more background

--- src/MainAppBar.tsx
+++ src/MainAppBar.tsx
@@ -7,7 +7,7 @@ export default function App() {
         <AppBar title="Home Page" position="sticky" sx={{ bgcolor: 'background.paper', color: 'text.primary'}}>
             <Toolbar>
                 <Typography variant="h6" component="div">
-                    Hehe
+                    SaltenLAN 2023 presents... Rocket League w/ Asle!!
                 </Typography>
             </Toolbar>
         </AppBar>
